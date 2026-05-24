47d95b50e607fa87b8e0395eae35e1cccd10e062
deng-rui <dr@der.kim>
2026-05-25 07:12:57 +0800

    test(injector): 增加表达式值修改命中契约参数用例

--- src/test/kotlin/kim/der/asm/FrameworkReliabilityTest.kt
+++ src/test/kotlin/kim/der/asm/FrameworkReliabilityTest.kt
@@ -1159,6 +1159,15 @@ class FrameworkReliabilityTest {
     }
 
     @Test
+    fun modifyExpressionValueExposesCountContractParameters() {
+        val methods = ModifyExpressionValue::class.java.declaredMethods.associateBy { it.name }
+
+        assertEquals(Int::class.javaPrimitiveType, methods["require"]?.returnType)
+        assertEquals(Int::class.javaPrimitiveType, methods["expect"]?.returnType)
+        assertEquals(Int::class.javaPrimitiveType, methods["allow"]?.returnType)
+    }
+
+    @Test
     fun modifyExpressionValueSliceLimitsInvokeReturnValueMatchesBetweenFromAndTo() {
         AsmRegistry.register(ModifyExpressionValueSliceMixin::class.java)
 
