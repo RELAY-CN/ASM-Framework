13f46f7e0428f2732b71ad4e1bb4a0ceebda05a1
deng-rui <dr@der.kim>
2026-05-25 00:30:29 +0800

    feat(injector): 支持操作包裹切片过滤

--- src/main/kotlin/kim/der/asm/injector/AsmInjectorFactory.kt
+++ src/main/kotlin/kim/der/asm/injector/AsmInjectorFactory.kt
@@ -126,6 +126,7 @@ object AsmInjectorFactory {
      * @param asmInfo ASM 注册信息
      * @param at 调用点定位；当前仅支持 INVOKE
      * @param ordinal 匹配调用点序号；负数表示处理全部匹配调用点
+     * @param slice 切片范围；当前 INVOKE 操作包裹支持 INVOKE 边界切片
      * @return WrapOperation 注入器
      *
      * @author Dr (dr@der.kim)
@@ -136,7 +137,8 @@ object AsmInjectorFactory {
         asmInfo: AsmInfo,
         at: At,
         ordinal: Int = -1,
-    ): AsmInjector = WrapOperationInjector(method, asmInfo, at, ordinal)
+        slice: Slice = Slice(),
+    ): AsmInjector = WrapOperationInjector(method, asmInfo, at, ordinal, slice)
 
     /**
      * 创建 WrapWithCondition 注入器。
