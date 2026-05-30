bdbf9056163e7aa61b699bb4ad9cdbc09b3ea11f
deng-rui <dr@der.kim>
2026-05-31 03:43:55 +0800

    docs(api): 补充描述符工具 KDoc

--- src/main/kotlin/kim/der/asm/utils/DescriptionUtil.kt
+++ src/main/kotlin/kim/der/asm/utils/DescriptionUtil.kt
@@ -7,12 +7,21 @@ import org.objectweb.asm.Type
 import java.lang.reflect.Method
 
 /**
- * Basically [Type.getDescriptor] since that class might not be
- * available at Runtime.
+ * JVM 描述符工具。
+ *
+ * 该工具提供与 [Type.getDescriptor] 等价的轻量描述符生成能力，供运行期桥接代码在不直接依赖 ASM `Type`
+ * 的场景下生成方法和类型描述符。
+ *
+ * @author Dr (dr@der.kim)
+ * @date 2025-11-24
  */
 @Suppress("UNUSED")
 object DescriptionUtil {
     private val PRIMITIVES: MutableMap<Class<*>?, String> = HashMap()
+
+    /**
+     * `java.lang.Object` 的 JVM internal name。
+     */
     const val ObjectClassName = "java/lang/Object"
 
     init {
@@ -27,6 +36,17 @@ object DescriptionUtil {
         PRIMITIVES[Void.TYPE] = "V"
     }
 
+    /**
+     * 生成反射方法的调用点描述。
+     *
+     * 返回值格式为 `methodName(paramDesc)returnDesc`，例如 `valueOf(I)Ljava/lang/String;`。
+     *
+     * @param method 反射方法
+     * @return 带方法名的 JVM 方法描述
+     *
+     * @author Dr (dr@der.kim)
+     * @date 2025-11-24
+     */
     @JvmStatic
     fun getDesc(method: Method): String {
         val desc = StringBuilder(method.name).append("(")
@@ -37,6 +57,17 @@ object DescriptionUtil {
         return desc.toString()
     }
 
+    /**
+     * 生成 Java 类型的 JVM 类型描述符。
+     *
+     * 基础类型返回单字符描述符，引用类型返回 `Linternal/name;`，数组类型会保留对应数量的 `[` 前缀。
+     *
+     * @param type Java 类型
+     * @return JVM 类型描述符
+     *
+     * @author Dr (dr@der.kim)
+     * @date 2025-11-24
+     */
     @JvmStatic
     fun getDesc(type: Class<*>): String {
         val result = StringBuilder()
